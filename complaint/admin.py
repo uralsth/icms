bd33ddc01e85f246cd279d375890be0f575e2dd4
uralsth <shresthaural100@gmail.com>
2022-07-21 21:02:29 +0545

    register compaint, department and staff model in admin panel

--- complaint/admin.py
+++ complaint/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Complaint, Department, Staff
 
 # Register your models here.
+admin.site.register(Complaint)
+admin.site.register(Department)
+admin.site.register(Staff)
