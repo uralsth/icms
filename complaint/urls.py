e73b90595ed73daf6440085fabf0c0264c198742
uralsth <shresthaural100@gmail.com>
2022-08-16 20:33:23 +0545

    added urlpatterns for complaint

--- complaint/urls.py
+++ complaint/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from . import views
 
 app_name = "complaint"
 
 urlpatterns = [
-
+    path("", views.complaint_home, name="home"),
+    path("list", views.complaint_list, name="list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:complaint>/",
+        views.complaint_detail,
+        name="detail",
+    ),
 ]
