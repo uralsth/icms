a1310c7890ba4185be571a68720f2f649770aff9
uralsth <shresthaural100@gmail.com>
2022-08-16 20:30:29 +0545

    admin panel configuration for accounts

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+# from .models import Profile
+from .models import CustomUser, Staff, Complainant
 
 # Register your models here.
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'photo')
+#     list_editable = ['phone_number','photo']
+#         return f'Profile for user {self.user.username}'
+
+# class CustomUserAdmin(UserAdmin):
+#     fieldsets = (
+#         *UserAdmin.fieldsets,
+#         (
+#          'Additonal Info',
+#          {
+#           'fields':('phone_number', 'photo', 'address')
+
+#           }
+#          )
+#     )
+
+fields = list(UserAdmin.fieldsets)
+fields[1] = (
+    "Personal Info",
+    {
+        "fields": (
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "photo",
+            "address",
+            'role',
+        )
+    },
+)
+UserAdmin.fieldsets = tuple(fields)
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Staff)
+admin.site.register(Complainant)
