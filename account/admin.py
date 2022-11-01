e067aa75d58b6108fa1c6ea12bd0e5d8c05f03f1
uralsth <shresthaural100@gmail.com>
2022-11-01 11:34:11 +0545

    added models in admin panel

--- account/admin.py
+++ account/admin.py
@@ -2,26 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 # from .models import Profile
-from .models import CustomUser, Staff, Complainant
-
-# Register your models here.
-# @admin.register(CustomUser)
-# class CustomUserAdmin(UserAdmin):
-#     list_display = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'photo')
-#     list_editable = ['phone_number','photo']
-#         return f'Profile for user {self.user.username}'
-
-# class CustomUserAdmin(UserAdmin):
-#     fieldsets = (
-#         *UserAdmin.fieldsets,
-#         (
-#          'Additonal Info',
-#          {
-#           'fields':('phone_number', 'photo', 'address')
-
-#           }
-#          )
-#     )
+from .models import  CustomUser , StaffProfile, Complainant
 
 fields = list(UserAdmin.fieldsets)
 fields[1] = (
@@ -41,5 +22,11 @@ fields[1] = (
 UserAdmin.fieldsets = tuple(fields)
 
 admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Staff)
-admin.site.register(Complainant)
+
+@admin.register(StaffProfile)
+class StaffAdmin(admin.ModelAdmin):
+    exclude = ('user',)
+
+@admin.register(Complainant)
+class ComplainantAdmin(admin.ModelAdmin):
+    exclude = ('role',)
